1afe7584c5763ea2490895817acbbfbe37f57d63
Fernando Lira <it.fernandolira@gmail.com>
2022-03-26 08:04:37 +0000

    Databinding - Binding to Custom Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   serverElements: {
     type: string;
     name: string;
     content: string;
-  }[] = [{type: 'server', name: 'Server Test', content: 'Just a test!'}];
+  }[] = [{ type: 'server', name: 'Server Test', content: 'Just a test!' }];
 
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintCreated(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
